a06016a8901f730ce4e338d89c315a96692f00f0
Joydeep Mukherjee <joydeepm02@gmail.com>
2020-11-22 11:20:00 -0500

    Add functions for platform generation and removal

--- include/core/game.h
+++ include/core/game.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <list>
+
 #include <core/platform.h>
 #include <core/player.h>
 #include <physics/interactions.h>
@@ -26,7 +28,7 @@ class Game {
   Game();
   GameStatus GetGameStatus() const;
   Player GetPlayer() const;
-  std::vector<Platform> GetPlatforms() const;
+  std::list<Platform> GetPlatforms() const;
   Platform GetGroundPlatform() const;
   void SetGameStatus(GameStatus game_status);
 
@@ -54,11 +56,14 @@ class Game {
   void PlayerJump(size_t desired_location);
 
  private:
-  void Collide(Player& player, Platform& platform, double dt);
+  void CollidePlayerWithPlatforms(double dt);
   void CheckGameOver(double dt);
+  void GenerateNextPlatform();
+  void RemoveOldPlatform();
+  bool PlatformGoingOffScreen(const Platform& platform);
   GameStatus game_status_;
   Player player_;
-  std::vector<Platform> platforms_;
+  std::list<Platform> platforms_;
   Platform ground_platform_;
   const size_t kMaxPlayerXVelocity = 50;
   const size_t kPointerToVelocityScale = 50;
--- src/core/game.cc
+++ src/core/game.cc
@@ -21,7 +21,7 @@ Game::Game() {
   platforms_.emplace_back(glm::dvec2(600, 300), glm::dvec2(0, -0.001), 100, 10);
   platforms_.emplace_back(glm::dvec2(400, 350), glm::dvec2(0, -0.001), 100, 10);
   platforms_.emplace_back(glm::dvec2(400, 450), glm::dvec2(0, -0.001), 20, 60);
-  platforms_.emplace_back(glm::dvec2(400, 20), glm::dvec2(0, -0.001), 400, 10);
+  platforms_.emplace_back(glm::dvec2(400, 20), glm::dvec2(0, -0.1), 400, 10);
   ground_platform_ = Platform(glm::dvec2(437.5, 5), glm::dvec2(0, 0.001), 875,
                               10);
 }
@@ -34,7 +34,7 @@ Player Game::GetPlayer() const {
   return player_;
 }
 
-std::vector<Platform> Game::GetPlatforms() const {
+std::list<Platform> Game::GetPlatforms() const {
   return platforms_;
 }
 
@@ -48,13 +48,10 @@ void Game::SetGameStatus(GameStatus game_status) {
 
 void Game::UpdateState(double dt) {
   if (game_status_ == IN_PROGRESS) {
-    for (Platform& platform : platforms_) {
-      Collide(player_, platform, dt);
-      platform.UpdateState(dt);
-    }
+    CollidePlayerWithPlatforms(dt);
+    GenerateNextPlatform();
+    RemoveOldPlatform();
     CheckGameOver(dt);
-    ground_platform_.UpdateState(dt);
-    player_.UpdateState(dt);
   }
 }
 
@@ -62,8 +59,8 @@ void Game::MovePlayer(size_t desired_location) {
   double new_x_velocity = (desired_location - player_.GetPosition().x) /
                           kPointerToVelocityScale;
   new_x_velocity = abs(new_x_velocity) > kMaxPlayerXVelocity
-                     ? kMaxPlayerXVelocity * (new_x_velocity < 0 ? -1 : 1)
-                     : new_x_velocity;
+                   ? kMaxPlayerXVelocity * (new_x_velocity < 0 ? -1 : 1)
+                   : new_x_velocity;
   player_.SetVelocity(
           glm::dvec2(
                   new_x_velocity,
@@ -86,45 +83,54 @@ void Game::PlayerJump(size_t desired_location) {
   }
 }
 
-void Game::Collide(Player& player, Platform& platform, double dt) {
+void Game::CollidePlayerWithPlatforms(double dt) {
   glm::dvec2 new_position = player_.GetPosition();
-  switch (physics::interactions::DetermineCollision(player_, platform, dt)) {
-    case physics::interactions::CollisionType::NoCollision:
-      player_.SetOnGround(false);
-      break;
-    case physics::interactions::CollisionType::PlayerOnPlatform:
-      player_.SetJumping(false);
-      player_.SetOnGround(true);
-      new_position.y = platform.GetPosition().y +
-                       (platform.GetHeight() / 2) +
-                       (player_.GetHeight() / 2);
-      player_.SetPosition(new_position);
-      player_.SetVelocity(glm::dvec2(player_.GetVelocity().x,
-                                     platform.GetVelocity().y));
-      break;
-    case physics::interactions::CollisionType::PlayerUnderPlatform:
-      new_position.y = platform.GetPosition().y -
-                       (platform.GetHeight() / 2) -
-                       (player_.GetHeight() / 2);
-      player_.SetPosition(new_position);
-      player_.SetVelocity(glm::dvec2(player_.GetVelocity().x,
-                                     -player_.GetVelocity().y));
-      break;
-    case physics::interactions::CollisionType::PlayerOnLeftOfPlatform:
-      new_position.x = platform.GetPosition().x -
-                       (platform.GetWidth() / 2) -
-                       (player_.GetWidth() / 2);
-      player_.SetPosition(new_position);
-      player_.SetVelocity(glm::dvec2(0, player_.GetVelocity().y));
-      break;
-    case physics::interactions::CollisionType::PlayerOnRightOfPlatform:
-      new_position.x = platform.GetPosition().x +
-                       (platform.GetWidth() / 2) +
-                       (player_.GetWidth() / 2);
-      player_.SetPosition(new_position);
-      player_.SetVelocity(glm::dvec2(0, player_.GetVelocity().y));
-      break;
+  for (Platform& platform : platforms_) {
+    switch (physics::interactions::DetermineCollision(player_, platform, dt)) {
+      case physics::interactions::CollisionType::NoCollision:
+        player_.SetOnGround(false);
+        break;
+      case physics::interactions::CollisionType::PlayerOnPlatform:
+        player_.SetJumping(false);
+        player_.SetOnGround(true);
+        new_position.y = platform.GetPosition().y +
+                         (platform.GetHeight() / 2) +
+                         (player_.GetHeight() / 2);
+        player_.SetPosition(new_position);
+        player_.SetVelocity(glm::dvec2(player_.GetVelocity().x,
+                                       platform.GetVelocity().y));
+        break;
+      case physics::interactions::CollisionType::PlayerUnderPlatform:
+        new_position.y = platform.GetPosition().y -
+                         (platform.GetHeight() / 2) -
+                         (player_.GetHeight() / 2);
+        player_.SetPosition(new_position);
+        player_.SetVelocity(glm::dvec2(player_.GetVelocity().x,
+                                       -player_.GetVelocity().y));
+        break;
+      case physics::interactions::CollisionType::PlayerOnLeftOfPlatform:
+        new_position.x = platform.GetPosition().x -
+                         (platform.GetWidth() / 2) -
+                         (player_.GetWidth() / 2);
+        player_.SetPosition(new_position);
+        player_.SetVelocity(glm::dvec2(0, player_.GetVelocity().y));
+        break;
+      case physics::interactions::CollisionType::PlayerOnRightOfPlatform:
+        new_position.x = platform.GetPosition().x +
+                         (platform.GetWidth() / 2) +
+                         (player_.GetWidth() / 2);
+        player_.SetPosition(new_position);
+        player_.SetVelocity(glm::dvec2(0, player_.GetVelocity().y));
+        break;
+    }
+    platform.UpdateState(dt);
   }
+  if (physics::interactions::DetermineCollision(player_, ground_platform_, dt)
+      != physics::interactions::NoCollision) {
+    game_status_ = GAME_OVER_SCREEN;
+  }
+  ground_platform_.UpdateState(dt);
+  player_.UpdateState(dt);
 }
 
 void Game::CheckGameOver(double dt) {
@@ -132,6 +138,22 @@ void Game::CheckGameOver(double dt) {
       != physics::interactions::NoCollision) {
     game_status_ = GAME_OVER_SCREEN;
   }
+}
+
+void Game::GenerateNextPlatform() {
+  Platform last_platform = *std::prev(platforms_.end());
+}
+
+void Game::RemoveOldPlatform() {
+  for (auto it = platforms_.begin(); it != platforms_.end(); ++it) {
+    if (PlatformGoingOffScreen(*it)) {
+      platforms_.erase(it);
+    }
+  }
+}
+
+bool Game::PlatformGoingOffScreen(const Platform& platform) {
+  return platform.GetPosition().y < ground_platform_.GetPosition().y;
 }
 
 }
